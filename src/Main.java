68acc35721179b84234f46e84822074e8a38ee82
OldWangWet <3060124180@qq.com>
2024-10-21 16:03:42 +0800

    2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hell!");
+        System.out.printf("Hel!");
 
 
     }
